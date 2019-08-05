109a2c341704a982be7829e59c4d417f8ce727df
ChuChu999 <nchu999@gmail.com>
2019-08-05 13:42:06 -0500

    initial commit

---
+++ tw_actions.py
@@ -0,0 +1,233 @@
+from datetime import datetime
+from enum import Enum
+import os
+import random
+import re
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+
+
+class Site(Enum):
+    STAGING = "https://cf-staging.takewalks.com"
+    PRODUCTION = "https://www.takewalks.com"
+
+
+class General:
+
+    def set_up_selenium(self):
+        self.browser = webdriver.Chrome()
+        self.wait = WebDriverWait(self.browser, 30)
+
+    def tear_down_selenium(self):
+        self.browser.quit()
+
+    def initialize_test(self, test_name: str, url: str):
+        self.test_name = test_name
+        self.url = url
+        self.screenshot_dir = "Screenshots/" + self.test_name
+
+        os.makedirs(self.screenshot_dir, exist_ok=True)
+
+    def initialize_action_chains(self):
+        self.actions = ActionChains(self.browser)
+
+    def save_screenshot(self, file_name: str):
+        self.browser.save_screenshot(
+            self.screenshot_dir + "/" + str(datetime.now()) + " - " + file_name)
+
+    def focus(self, element: webdriver.remote.webelement.WebElement):
+        self.browser.execute_script("arguments[0].focus();", element)
+
+    def load_site(self):
+        self.browser.get(self.url)
+        assert "Take Walks" in self.browser.title
+
+    def select_market(self):
+        region = self.wait.until(
+            EC.presence_of_element_located((By.LINK_TEXT, "European Tours")))
+        market = self.wait.until(
+            EC.presence_of_element_located((By.LINK_TEXT, "Rome")))
+
+        self.initialize_action_chains()
+        self.save_screenshot("Pre-Market.png")
+        self.actions.move_to_element(region).click(market).perform()
+        self.save_screenshot("Post-Market.png")
+
+    def select_tour(self):
+        tour_overview = self.wait.until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, "div.tour-list-items.vertical")))
+        tours = tour_overview.find_elements_by_tag_name("a")
+        tour = random.choice(tours)
+
+        self.save_screenshot("Pre-Tour.png")
+        tour.send_keys(Keys.ENTER)
+        self.save_screenshot("Post-Tour.png")
+
+    def increment_month(self):
+        for i in range(2):
+            next_month = self.wait.until(EC.element_to_be_clickable(
+                (By.CSS_SELECTOR, "div.right-book-desktop a.datepick-cmd-next")))
+
+            next_month.click()
+            time.sleep(0.25)
+
+    def select_date(self):
+        calendar = self.wait.until(EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, "div.right-book-desktop div.datepick-month")))
+        weeks = calendar.find_elements_by_tag_name("tr")
+        availableDays = []
+
+        print(len(availableDays))
+
+        for week in weeks:
+            availableDays.extend(week.find_elements_by_tag_name("a"))
+
+            if len(availableDays) > 0:
+                break
+
+        if not availableDays:
+            raise Exception("no available days two months in advance")
+
+        self.wait.until(EC.visibility_of(availableDays[0]))
+        self.save_screenshot("Pre-Date.png")
+        availableDays[0].send_keys(Keys.ENTER)
+
+    def select_time(self):
+        time_select = Select(self.wait.until(EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, 'div.right-book-desktop select[name="time"]'))))
+
+        time_select.select_by_index(1)
+
+    def select_adults(self):
+        adults = Select(self.wait.until(EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, 'div.right-book-desktop select[name="adults"]'))))
+
+        adults.select_by_index(1)
+
+    def book_tour(self):
+        book = self.wait.until(EC.element_to_be_clickable(
+            (By.CSS_SELECTOR, "div.right-book-desktop button.btn-book-now")))
+
+        self.save_screenshot("Pre-Book.png")
+        book.send_keys(Keys.ENTER)
+        self.save_screenshot("Post-Book.png")
+
+    def fill_first_name(self):
+        first_name = self.wait.until(
+            EC.visibility_of_element_located((By.ID, "theFirstname")))
+
+        first_name.send_keys("Test")
+
+    def fill_last_name(self):
+        last_name = self.browser.find_element_by_id("theLastname")
+
+        last_name.send_keys("Booking")
+
+    def fill_email(self):
+        email = self.browser.find_element_by_id("theEmail")
+
+        email.send_keys("nicholas+staging@takewalks.com")
+
+    def fill_phone(self):
+        phone = self.browser.find_element_by_id("thePhone")
+
+        phone.send_keys("1234567")
+        self.save_screenshot("Post-Phone.png")
+
+    def fill_credit_card(self):
+        cc = self.browser.find_element_by_id("ccno")
+
+        cc.send_keys("5555555555554444")
+
+    def select_expiry_month(self):
+        cc_month = Select(self.browser.find_element_by_name("ccMonth"))
+
+        cc_month.select_by_value("12")
+
+    def select_expiry_year(self):
+        cc_year = Select(self.browser.find_element_by_name("ccYear"))
+
+        cc_year.select_by_value("2022")
+
+    def fill_cvv(self):
+        cvv = self.browser.find_element_by_id("theCcv")
+
+        cvv.send_keys("123")
+        self.save_screenshot("Post-CVV.png")
+
+    def fill_address(self):
+        address = self.browser.find_element_by_name("street_address")
+
+        address.send_keys("5323 Levander Loop")
+
+    def select_country(self):
+        country = Select(self.browser.find_element_by_name("country"))
+
+        country.select_by_value("United States of America")
+
+    def fill_zip_code(self):
+        zip = self.browser.find_element_by_name("zip")
+
+        zip.send_keys("78702")
+
+    def select_state(self):
+        state = Select(self.browser.find_element_by_id("state_select"))
+
+        state.select_by_value("TX")
+
+    def fill_city(self):
+        city = self.browser.find_element_by_name("city")
+
+        city.send_keys("Austin")
+
+    def tick_terms_and_conditions(self):
+        terms = self.browser.find_element_by_css_selector(
+            'label[for="ccb_01"]')
+
+        terms.click()
+
+    def submit_payment_form(self):
+        pay = self.browser.find_element_by_class_name("complete_booking")
+
+        self.save_screenshot("Pre-Pay.png")
+        pay.click()
+        self.save_screenshot("Post-Pay.png")
+
+        booking_confirmation = self.wait.until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, "h1.page-sub-title")))
+        booking_id = "".join(re.findall(r"\d", booking_confirmation.text))
+
+        self.save_screenshot("BookingConfirmation - " + booking_id + ".png")
+
+
+class PromoCode:
+
+    def __init__(self, general: General):
+        self.browser = general.browser
+        self.wait = general.wait
+        self.save_screenshot = general.save_screenshot
+
+    def fill_promo_code(self):
+        promo_code_box = self.wait.until(
+            EC.element_to_be_clickable((By.LINK_TEXT, "PROMO CODE")))
+
+        self.save_screenshot("Pre-PromoBox.png")
+        promo_code_box.send_keys(Keys.ENTER)
+        self.save_screenshot("Post-PromoBox.png")
+
+        promo_code = self.wait.until(
+            EC.visibility_of_element_located((By.NAME, "promo")))
+
+        promo_code.send_keys("rc10")
+        self.save_screenshot("Post-PromoCode.png")
+
+        apply_promo = self.browser.find_element_by_class_name("promo-btn")
+        # self.save_screenshot("Post-ApplyPromo.png")
+
+        apply_promo.send_keys(Keys.ENTER)
---
+++ tw_tests.py
@@ -0,0 +1,89 @@
+import tw_actions
+import time
+import unittest
+
+
+class TWTests(unittest.TestCase):
+
+    def setUp(self):
+        self.general = tw_actions.General()
+        self.site = tw_actions.Site
+
+        self.general.set_up_selenium()
+
+    def tearDown(self):
+        self.general.tear_down_selenium()
+
+    def test_booking(self):
+        self.general.initialize_test("TestBooking", self.site.STAGING.value)
+        self.general.load_site()
+        self.general.select_market()
+        self.general.select_tour()
+        self.general.increment_month()
+        self.general.select_date()
+        self.general.select_time()
+        self.general.select_adults()
+        self.general.book_tour()
+        self.general.fill_first_name()
+        self.general.fill_last_name()
+        self.general.fill_email()
+        self.general.fill_phone()
+        self.general.fill_credit_card()
+        self.general.select_expiry_month()
+        self.general.select_expiry_year()
+        self.general.fill_cvv()
+        self.general.fill_address()
+        self.general.select_country()
+        self.general.fill_zip_code()
+        self.general.select_state()
+        self.general.fill_city()
+        self.general.tick_terms_and_conditions()
+        self.general.submit_payment_form()
+        time.sleep(3)
+
+    def test_promo_code(self):
+        self.promo = tw_actions.PromoCode(self.general)
+
+        self.general.initialize_test("TestPromoCode", self.site.STAGING.value)
+        self.general.load_site()
+        self.general.select_market()
+        self.general.select_tour()
+        self.general.increment_month()
+        self.general.select_date()
+        self.general.select_time()
+        self.general.select_adults()
+        self.general.book_tour()
+        self.promo.fill_promo_code()  # fill promo code
+        self.general.fill_first_name()
+        self.general.fill_last_name()
+        self.general.fill_email()
+        self.general.fill_phone()
+        self.general.fill_credit_card()
+        self.general.select_expiry_month()
+        self.general.select_expiry_year()
+        self.general.fill_cvv()
+        self.general.fill_address()
+        self.general.select_country()
+        self.general.fill_zip_code()
+        self.general.select_state()
+        self.general.fill_city()
+        self.general.tick_terms_and_conditions()
+        self.general.submit_payment_form()
+        time.sleep(3)
+
+
+def run_test(test_name: str):
+    suite = unittest.TestSuite()
+    runner = unittest.TextTestRunner()
+
+    suite.addTest(TWTests(test_name))
+    runner.run(suite)
+
+
+def run_tests():
+    unittest.main()
+
+
+if __name__ == "__main__":
+    # run_test("test_promo_code")
+    run_tests()
